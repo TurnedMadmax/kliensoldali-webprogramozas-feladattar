0f4dbd2d0de1793408393b2200d59e788df7b26f
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2023-03-24 23:05:25 +0100

    hello-world

--- hello-world/src/index.jsx
+++ hello-world/src/index.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+import "./index.css";
 
-// ReactDOM.render(
-//   <Hello />,
-//   document.getElementById('root')
-// );
+// ReactDOM.createRoot(document.getElementById("root")).render(<Hello />);
