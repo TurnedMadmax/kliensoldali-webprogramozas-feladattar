5730074c2016972f1f9bea0d316f49886d5abaa0
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2023-03-25 12:48:14 +0100

    hangman

--- hangman/src/Letter.js
+++
@@ -1,9 +0,0 @@
-import cn from "classnames";
-
-const Letter = ({ missing, visible, children }) => (
-  <span className={cn({hianyzo: missing})}>
-    {visible && children}
-  </span>
-)
-
-export default Letter;
