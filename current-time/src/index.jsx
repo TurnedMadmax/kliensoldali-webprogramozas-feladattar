252d9c815bbebfbd15e1bb40eb2ede67277f1524
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2023-03-25 10:35:52 +0100

    rewrite index.js

--- current-time/src/index.jsx
+++ current-time/src/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode></React.StrictMode>);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<React.StrictMode></React.StrictMode>);
--- hello-world/src/index.jsx
+++ hello-world/src/index.jsx
@@ -1,5 +1,11 @@
 // import React from "react";
-// import ReactDOM from "react-dom/client";
+// import { createRoot } from "react-dom/client";
 import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(<Hello />);
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
