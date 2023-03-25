abf2219533534f1ce0e2d92263b9b21f78478820
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2023-03-25 13:01:52 +0100

    mini-login

--- mini-login/src/Login.jsx
+++ mini-login/src/Login.jsx
@@ -1,23 +1,12 @@
+// eslint-disable-next-line react/prop-types, no-unused-vars
 const Login = ({ login }) => {
   return (
     <form>
       <label htmlFor="username">Felhasználónév: </label>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        value=""
-        label="Felhasználónév"
-      />
+      <input type="text" id="username" name="username" value="" label="Felhasználónév" />
       <br />
       <label htmlFor="password">Jelszó: </label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        value=""
-        label="Jelszó"
-      />
+      <input type="password" id="password" name="password" value="" label="Jelszó" />
       <br />
       <button type="submit"> Elküld</button>
     </form>
